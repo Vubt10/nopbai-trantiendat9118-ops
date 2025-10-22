7892f147a0bce646705e833b416e79b96fb71db1
Trần Tiến Đạt <trantiendat9118@gmail.com>
2025-10-22 23:36:03 +0700

    lab8

---
+++ LAB_8/Bai1_Lab8.sql
@@ -0,0 +1,23 @@
+﻿-- Bước 3.2 Thực hiện Full Backup
+BACKUP DATABASE AP
+TO DISK = 'D:\SQL\BackupCSDL\APFull.bak'
+WITH FORMAT,
+NAME = 'Full Backup of AP';
+-- Bước 4: Thêm bảng mới Test để kiểm tra:
+USE AP;
+CREATE TABLE Test (
+    ID INT PRIMARY KEY,
+    Name NVARCHAR(50)
+);
+-- Bước 5: Phục hồi lại CSDL từ bản Full Backup:
+USE master;
+RESTORE DATABASE AP
+FROM DISK = 'D:\SQL\BackupCSDL\APFull.bak'
+WITH REPLACE;
+
+ -- Bước 6: Kiểm tra lại — bảng Test không còn tồn tại  phục hồi thành công.
+USE AP;
+SELECT * 
+FROM sys.tables
+WHERE name = 'Test';
+
---
+++ LAB_8/bai2_lab8.sql
@@ -0,0 +1,55 @@
+﻿-- Bước 1: Tạo bản sao Full Backup:
+BACKUP DATABASE AP
+TO DISK = 'D:\SQL\BackupCSDL\APFull.bak'
+WITH FORMAT,
+NAME = 'Full Backup APFull1';
+
+-- Bước 2: Thêm bảng Test1:
+USE AP;
+CREATE TABLE Test1 (ID INT, Name NVARCHAR(50));
+
+-- Bước 3: Tạo bản Differential Backup đầu tiên:
+BACKUP DATABASE AP
+TO DISK = 'D:\SQL\BackupCSDL\APDiff1.bak'
+WITH DIFFERENTIAL,
+NAME = 'Differential Backup APDiff1';
+
+-- Bước 4: Thêm bảng Test2:
+CREATE TABLE Test2 (ID INT, Name NVARCHAR(50));
+
+-- Bước 5: Tạo bản Differential Backup thứ hai:
+BACKUP DATABASE AP
+TO DISK = 'D:\SQL\BackupCSDL\APDiff2.bak'
+WITH DIFFERENTIAL,
+NAME = 'Differential Backup APDiff2';
+
+--Bước 7: Phục hồi từ Full + Diff1:
+USE master;
+RESTORE DATABASE AP
+FROM DISK = 'D:\SQL\BackupCSDL\APFull1.bak'
+WITH NORECOVERY;
+RESTORE DATABASE AP
+FROM DISK = 'D:\SQL\BackupCSDL\APFull.bak'
+WITH RECOVERY;
+
+-- Bước 8: Phục hồi từ Full + Diff2:
+USE master;
+RESTORE DATABASE AP
+FROM DISK = 'D:\SQL\BackupCSDL\APFull.bak'
+WITH NORECOVERY;
+RESTORE DATABASE AP
+FROM DISK = 'D:\SQL\BackupCSDL\APDiff2.bak'
+WITH RECOVERY;
+
+-- Bước 9: Thử phục hồi dùng APFull.bak (Bài 1) + APDiff1.bak → báo lỗi do không khớp chuỗi backup.
+USE master;
+-- Restore bản Full đầu tiên (bài 1)
+RESTORE DATABASE AP
+FROM DISK = 'D:\SQL\BackupCSDL\APFull.bak'
+WITH NORECOVERY;
+
+-- Restore bản Diff của bài 2 (không cùng chuỗi)
+RESTORE DATABASE AP
+FROM DISK = 'D:\SQL\BackupCSDL\APDiff1.bak'
+WITH RECOVERY;
+
